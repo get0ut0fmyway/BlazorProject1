e3e77efb126f55e502da17c6a6601df1affa08b3
get0ut0fmyway <bresonate@gmail.com>
2020-04-04 16:28:02 -0400

    finish edit form and edit nav link

--- BlazorProject1.Server/Pages/EmployeeEditBase.cs
+++ BlazorProject1.Server/Pages/EmployeeEditBase.cs
@@ -13,7 +13,10 @@ namespace BlazorProject1.Server.Pages
         public IEmployeeDataService EmployeeDataService { get; set; }
 
         [Inject]
-        public ICountryDataService CountryDataService{ get; set; }
+        public ICountryDataService CountryDataService { get; set; }
+
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
 
         [Parameter]
         public string EmployeeId { get; set; }
@@ -21,17 +24,20 @@ namespace BlazorProject1.Server.Pages
         public Employee Employee { get; set; } = new Employee();
 
         public List<Country> Countries { get; set; } = new List<Country>();
+        
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+        
         protected string _countryId = string.Empty;
-
-        protected string _jobCategory = string.Empty;
+        protected string _jobCategoryId = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
             Countries = (await CountryDataService.GetAllCountries()).ToList();
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
 
             _countryId = Employee.CountryId.ToString();
-            _jobCategory = Employee.JobCategoryId.ToString();
+            _jobCategoryId = Employee.JobCategoryId.ToString();
         }
     }
 }
