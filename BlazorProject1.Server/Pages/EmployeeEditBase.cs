be7626be3cdea41c40ef01356c33a4fc4e0a5dc9
get0ut0fmyway <bresonate@gmail.com>
2020-04-05 15:25:44 -0400

    make form better, clauses for new and existing employee, new navlink

--- BlazorProject1.Server/Pages/EmployeeEditBase.cs
+++ BlazorProject1.Server/Pages/EmployeeEditBase.cs
@@ -1,6 +1,7 @@
 ﻿using BethanysPieShopHRM.Shared;
 using BlazorProject1.Server.Services;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,14 +31,61 @@ namespace BlazorProject1.Server.Pages
         protected string _countryId = string.Empty;
         protected string _jobCategoryId = string.Empty;
 
+        protected string _message = string.Empty;
+        protected string _statusClass = string.Empty;
+        protected bool _saved;
+
         protected override async Task OnInitializedAsync()
         {
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            _saved = false;
+
+            //Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
             Countries = (await CountryDataService.GetAllCountries()).ToList();
             JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
 
+            int.TryParse(EmployeeId, out var employeeId);
+            if (employeeId == 0) // new employee
+            {
+                Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now };
+            }
+            else // existing employee
+            {
+                Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            }
+
             _countryId = Employee.CountryId.ToString();
             _jobCategoryId = Employee.JobCategoryId.ToString();
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            Employee.CountryId = int.Parse(_countryId);
+            Employee.JobCategoryId = int.Parse(_jobCategoryId);
+
+            if (Employee.EmployeeId == 0) // new employee
+            {
+                var addedEmployee = await EmployeeDataService.AddEmployee(Employee);
+                if (addedEmployee != null)
+                {
+                    _statusClass = "alert-sucess";
+                    _message = "New employee added successfully.";
+                    _saved = true;
+                }
+                else
+                {
+                    _statusClass = "alert-danger";
+                    _message = "Something went wrong adding the new employee.  Please try again.";
+                    _saved = false;
+                }
+            }
+            else // existing employee
+            {
+                await EmployeeDataService.UpdateEmployee(Employee);
+                _statusClass = "alert-sucess";
+                _message = "Employee updated successfully.";
+                _saved = true;
+
+            }
         }
     }
 }
